2a5f7e90e34c61d9ba6624cef7df99c8d7bd4c98
Vinh Trần Hiển <tomoki159369@gmail.com>
2022-10-14 11:36:41 +0700

    final

--- Core/User/src/handle_interrupt.c
+++ Core/User/src/handle_interrupt.c
@@ -14,7 +14,6 @@ osThreadId_t AUXHandle, CountingHandle;
 osSemaphoreId_t xSemaphore1, xSemaphore2, xSemaphore3, xSemaphore4,
 				xSemaphore5, xSemaphore6, xSemaphore7, xSemaphore8;
 
-osMessageQueueId_t queue_flag;
 osMessageQueueId_t queue_count;
 
 /* Private function prototypes -----------------------------------------------*/
@@ -22,7 +21,7 @@ static void startHandleInterrupt(void * argument);
 static void counting(void * argument);
 
 /* Private functions ---------------------------------------------------------*/
-static void semaphore_init(void){
+static void init_queue_semaphore(void){
 	xSemaphore1 = osSemaphoreNew(1, 0, NULL);
 	xSemaphore2 = osSemaphoreNew(1, 0, NULL);
 	xSemaphore3 = osSemaphoreNew(1, 0, NULL);
@@ -31,6 +30,7 @@ static void semaphore_init(void){
 	xSemaphore6 = osSemaphoreNew(1, 0, NULL);
 	xSemaphore7 = osSemaphoreNew(1, 0, NULL);
 	xSemaphore8 = osSemaphoreNew(1, 0, NULL);
+	queue_count = osMessageQueueNew(1, sizeof(uint8_t), NULL);
 }
 
 void handle_AUX_signal_init(void){
@@ -49,65 +49,56 @@ void handle_AUX_signal_init(void){
 	};
 	CountingHandle = osThreadNew(counting, NULL, &countingTask);
 	(void)CountingHandle;
-	semaphore_init();
-	queue_flag = osMessageQueueNew(1, sizeof(bool), NULL);
-	queue_count = osMessageQueueNew(1, sizeof(uint8_t), NULL);
+	init_queue_semaphore();
+
 }
 
 static void counting(void * argument){
-	uint8_t count = 0;
-	bool flag = false;
+	static uint8_t count = 0;
 	while (1){
 		if (HAL_GPIO_ReadPin(BT_GPIO_Port, BT_Pin) == 0){
 			osDelay(100);
 			if (HAL_GPIO_ReadPin(BT_GPIO_Port, BT_Pin) == 0){
 				count++;
 				if (count > 8) count = 0;
-				flag = true;
+				osMessageQueuePut(queue_count, &count, 0, 0);
 			}
-			else flag = false;
-			osMessageQueuePut(queue_flag,  &flag,  0, 0);
-			osMessageQueuePut(queue_count, &count, 0, 0);
 		}
 	}
 }
 
 static void startHandleInterrupt(void * argument){
+	osStatus_t countStatus;
 	uint8_t xCount;
-	bool xFlag;
-	osStatus_t flagStatus, countStatus;
 	while (1){
-		flagStatus = osMessageQueueGet(queue_flag, &xFlag, 0, 0);
 		countStatus = osMessageQueueGet(queue_count, &xCount, 0, 0);
-		if ((flagStatus == osOK) && (flagStatus == osOK))
+		if (countStatus == osOK)
 		{
-			if (xFlag) {
-				switch (xCount){
-					case 1:
-						osSemaphoreRelease(xSemaphore1);
-						break;
-					case 2:
-						osSemaphoreRelease(xSemaphore2);
-						break;
-					case 3:
-						osSemaphoreRelease(xSemaphore3);
-						break;
-					case 4:
-						osSemaphoreRelease(xSemaphore4);
-						break;
-					case 5:
-						osSemaphoreRelease(xSemaphore5);
-						break;
-					case 6:
-						osSemaphoreRelease(xSemaphore6);
-						break;
-					case 7:
-						osSemaphoreRelease(xSemaphore7);
-						break;
-					case 8:
-						osSemaphoreRelease(xSemaphore8);
-						break;
-				}
+			switch (xCount){
+				case 1:
+					osSemaphoreRelease(xSemaphore1);
+					break;
+				case 2:
+					osSemaphoreRelease(xSemaphore2);
+					break;
+				case 3:
+					osSemaphoreRelease(xSemaphore3);
+					break;
+				case 4:
+					osSemaphoreRelease(xSemaphore4);
+					break;
+				case 5:
+					osSemaphoreRelease(xSemaphore5);
+					break;
+				case 6:
+					osSemaphoreRelease(xSemaphore6);
+					break;
+				case 7:
+					osSemaphoreRelease(xSemaphore7);
+					break;
+				case 8:
+					osSemaphoreRelease(xSemaphore8);
+					break;
 			}
 		}
 	}
